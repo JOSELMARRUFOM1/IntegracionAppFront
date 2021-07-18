e575803183168e7a834434e3e71c1ea6fce78a05
jonito <jonito_or@hotmail.com>
2021-07-18 12:11:39 -0300

    version antes del datateble

--- src/app/busqueda/busqueda.component.ts
+++ src/app/busqueda/busqueda.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
+import * as datamiddleware from '../../assets/json/Middleware.json';
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
 export class BusquedaComponent implements OnInit {
-
+  middelwares: any = (datamiddleware as any).default;
+  
   constructor() { }
 
+  formParametros = new FormGroup({
+    IdIdentificadorCI: new FormControl(''),
+    IdDescripcion: new FormControl(''),
+    Middelware: new FormControl(0),
+  });
   ngOnInit(): void {
+  }
+  editar():void {
+    console.log(this.formParametros.value);
   }
 
 }
